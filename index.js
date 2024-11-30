a4d07a47e660a664b6f7253774aa8bc77f127d5b
Mehroj Alimov <128947362+mehrojalimov@users.noreply.github.com>
2024-11-30 18:09:09 -0500

    setting up get and post

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const Vehicle = require('./Products/vehicle.product.js');
+
 const credentials = require('./env.json');
 
 app.use(express.json());
 
+//Add connection string from 4.4 and 4.5 instructions in Readme in env.json, repalcing with your own password and login
 const connectionString = `mongodb+srv://${credentials.Username}:${credentials.Password}@vehicleapi.ed7ug.mongodb.net/<your-database-name>?retryWrites=true&w=majority`;
 
 
-//Add that connection string from 4.4 instructions in Readme, then repalce with your own password and login
+app.get('/vehicle', async (req, res) => {
+    try {
+        const vehicle = await Vehicle.find({});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.post('/vehicle', async (req, res) => {
+    try {
+        const vehicle = await Vehicle.create(req.body);
+        res.status(201).json(vehicle);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+
+});
+
+
+
+
+
+
 mongoose.connect(connectionString)
 .then(() =>{
     console.log("Connected to database");
-    
-})
-
-
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+    app.listen(3000, ()=>{
+        console.log('Server is running on port 3000');
+    });
 })
